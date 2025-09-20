43634e98aac1b3089e002900f7ac56d7aa092402
Shohrux <shohruxnizomov170@gmail.com>
2025-09-20 12:01:38 +0500

    job larni bazaga saqlash un migrate va model yozildi

---
+++ app/Models/Job.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Job extends Model
+{
+    use HasFactory;
+    protected $fillable = [
+        'employer_id',
+        'title',
+        'description',
+        'type',
+        'contact_info',
+    ];
+
+    public function employer()
+    {
+        return $this->belongsTo(User::class, 'employer_id');
+    }
+}
